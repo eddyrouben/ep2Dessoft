65a503bde05d472148b95e2649f7f11f7d1c329d
eddyrouben <eddy.rouben@gmail.com>
2023-05-04 17:30:50 -0300

    define posicoes feito

--- defineposicoes.py
+++ defineposicoes.py
@@ -1,1 +1,10 @@
-print("Hello World")
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    lista = []
+    if orientacao == "vertical":
+        for i in range(tamanho):
+            lista.append([linha+i, coluna])
+        return lista
+    else:
+        for i in range(tamanho):
+            lista.append([linha, coluna+i])
+        return lista
